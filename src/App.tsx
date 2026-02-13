a4e0bf9a6a6fd584a8dc97ef6eaf34c59e435b95
rajgadade-cloud <raj.gadade@gmail.com>
2026-02-13 16:43:25 +0530

    Create React TypeScript hello world app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="container">
+      <h1>Hello World</h1>
+      <p>Welcome to your React + TypeScript app.</p>
+    </main>
+  )
+}
+
+export default App
